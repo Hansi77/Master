a0f4b3f1bf6be382b2ad016f3144cf05a0947e62
Hans Julian Halvorsen <hansjhal@stud.ntnu.no>
2021-04-14 13:48:48 +0200

    cleaned up the code

--- second_attempt.py
+++ second_attempt.py
@@ -41,7 +41,37 @@ def plotEdges(inputEdge,p):
     for edge in inputEdge:
         connectPoints(p[int(edge[0])],p[int(edge[1])],color = 'r') 
 
-def boundaries(points,N):
+################forskjellige domener og relaterte grenser##################
+
+#tradisjonellt rør
+def typeZeroDom(points,N):
+    i = 0
+    r = 1
+    elements = []
+    lin = []
+    sq = []
+    lin_vec = []
+    while r != (2**N)+1:
+        lin = np.asarray([i,i+2,i+(10*(2**N)+2),i+(10*(2**N)+4)])
+        sq = np.asarray([i,i+1,i+2,i+(5*(2**N)+1),i+(5*(2**N)+2),i+(5*(2**N)+3),i+(10*(2**N)+2),i+(10*(2**N)+3),i+(10*(2**N)+4)])
+        el = [lin,sq]
+        elements.append(el)
+        lin_vec = np.concatenate((lin_vec,lin))
+        i += 2
+        if i==(5*(2**N))*r + r -1:
+            i += 5*(2**N) +2
+            r += 2
+    return elements, np.array(list(set(lin_vec)),dtype=int)
+
+def typeZeroBdry(points,N):
+    non_homo_dir = [i*(5*(2**N)+1) for i in range(1,(2**N))]
+    homo_dir1 = [i for i in range(5*(2**N)+1)]
+    homo_dir2 = [i for i in range((2**N)*(5*(2**N)+1),((2**N)+1)*(5*(2**N)+1) )]
+    homo_neu = [i*(5*(2**N)+1)-1 for i in range(2,2**N+1)]
+    return np.asarray(non_homo_dir),np.asarray( np.concatenate((homo_dir1,homo_dir2))),np.asarray(homo_neu)
+
+#rør med utstikker
+def typeOneBdry(points,N):
     non_homo_dir = [i*(5*(2**N)+1) for i in range(1,(2**N))]
     homo_dir1 = [i for i in range(5*(2**N)+1)]
     homo_dir2 = [i for i in range((2**N)*(5*(2**N)+1),(2**N)*(5*(2**N)+1)+2*(2**N)+1)]
@@ -53,7 +83,7 @@ def boundaries(points,N):
     homo_neu = [i*(5*(2**N)+1)-1 for i in range(2,2**N+1)]
     return np.asarray(non_homo_dir), np.asarray(np.sort(homo_dir)), np.asarray(homo_neu)
 
-def hoodTaylor(points,N):
+def typeOneDom(points,N):
     i = 0
     r = 1
     elements = []
@@ -99,6 +129,7 @@ def hoodTaylor(points,N):
                 ti = i
     return elements, np.array(list(set(lin_vec)),dtype=int)
 
+#returnerer koeffisientene til de bikvadratiske basisfunksjonene
 def phiSq(points,el):
     phi_el = np.ones((9,9))
     for i,p in enumerate(points[el]):
@@ -112,6 +143,7 @@ def phiSq(points,el):
         basis_coeffs[a] = c
     return basis_coeffs
 
+#returnerer koeffisientene til de bilineære basisfunksjonene
 def phiLin(points,el):
     phi_el = np.ones((4,4))
     for i,p in enumerate(points[el]):
@@ -126,6 +158,7 @@ def phiLin(points,el):
         basis_coeffs[a] = c
     return basis_coeffs
 
+#numerisk integrasjonsfunksjon
 def gauss2D(integrand,p,el,i,j,c1 = 0,c2 = 0,multibasis = False,degree = 3):
     p1,p4 = p[el[0]], p[el[8]]
     a,b,c,d = p1[0], p4[0], p1[1], p4[1]
@@ -152,98 +185,7 @@ def gauss2D(integrand,p,el,i,j,c1 = 0,c2 = 0,multibasis = False,degree = 3):
                 integral += w1*w2*h1*h3*integrand(h1*ev1+h2,h3*ev2+h4,c2,i,j)
     return integral
 
-def stiffHelp(points,el,int_func,basis_type = 'lin'):
-    if basis_type == 'lin':
-        basis = phiLin(points,el)
-    else:
-        basis = phiSq(points,el)
-    dim = len(basis)
-    a_el = np.zeros((dim,dim))
-    for i in range(dim):
-        for j in range(dim):
-            a_el[i,j] += gauss2D(int_func,points,el,basis,i,j,basis_type)
-    return a_el
-
-def stiffnessMat(int_func,points,elements,basis_type = 'lin'):
-    A = sp.lil_matrix((len(points),len(points)))
-    for el in elements:
-        if basis_type == 'lin':
-            a_el = stiffHelp(points,el[0],int_func,basis_type)
-            for i,ai in enumerate(el[0]):
-                for j,aj in enumerate(el[0]):
-                    A[ai,aj] += a_el[i,j]
-        else:
-            a_el = stiffHelp(points,el[1],int_func,basis_type)
-            for i,ai in enumerate(el[1]):
-                for j,aj in enumerate(el[1]):
-                    A[ai,aj] += a_el[i,j]
-    return A
-
-def loadVec(int_func,points,elements,basis_type = 'lin'):
-    f = np.zeros((len(points)))
-    i = 0
-    for el in elements:
-        if basis_type == 'lin':
-            basis = phiLin(points,el[0])
-            for j,fj in enumerate(el[0]):
-                f[fj] += gauss2D(int_func,points,el[0],basis,i,j,basis_type=basis_type)
-        else:
-            basis = phiSq(points,el[1])
-            for j,fj in enumerate(el[1]):
-                f[fj] += gauss2D(int_func,points,el[1],basis,i,j,basis_type=basis_type)
-    return f
-
-def createLoadStiff(f_int,bilin_int,points,elements,lin_set = 0,basis_type = 'lin'):
-    non_homog_dir,homog_dir,neumann = boundaries(points,N)
-
-    loadpts = points[:,0] > .6999995
-    f = np.zeros((len(points)))
-    inner = np.array([i for i in range(len(points))])
-    if basis_type == 'lin':
-        lin_inv = np.array([i for i in range(len(points))])
-        lin_inv = lin_inv[~np.isin(np.arange(lin_inv.size),lin_set)]
-        non_homog_dir = non_homog_dir[np.isin(non_homog_dir,lin_set)]
-        homog_dir = homog_dir[np.isin(homog_dir,lin_set)]
-        f_temp = loadVec(f_int,points,elements,basis_type='lin')
-        
-
-        A = stiffnessMat(bilin_int,points,elements,basis_type='lin')
-        delete = np.array(list(set(np.concatenate((lin_inv,non_homog_dir,homog_dir)))))
-        inner = inner[~np.isin(np.arange(inner.size), delete)]
-        outer = np.concatenate((non_homog_dir,homog_dir))
-    else:
-        f_temp = loadVec(f_int,points,elements,basis_type='sq')
-        A = stiffnessMat(bilin_int,points,elements,basis_type='sq')
-        inner = inner[~np.isin(np.arange(inner.size), np.concatenate((non_homog_dir,homog_dir)))]
-        outer = np.concatenate((non_homog_dir,homog_dir))
-    
-    f[loadpts] = f_temp[loadpts]
-    A_inner = A[inner]
-    A_inner = A_inner[:,inner]
-    f_inner = f[inner]
-    B = A[inner]
-    B = B[:,outer]
-
-    rg_homo = np.zeros_like(homog_dir)
-    rg_non_homo = np.zeros_like(non_homog_dir)
-    rgy = points[non_homog_dir][:,1]
-    rg_non_homo[:] = 2000*rgy*(.2-rgy)
-    rg = np.concatenate((rg_non_homo,rg_homo))
-    A_inner = A_inner.tocsr()
-    B = B.tocsr()
-
-    rhs = f_inner - B@rg
-
-    u_bar = solver(A_inner,rhs)
-    u = np.zeros((len(points)))
-    u[inner] = u_bar
-    u[outer] = rg
-    if basis_type == 'lin':
-        u = u[lin_set]
-        return u, points[lin_set]
-    else:
-        return u,points
-
+#maskeringsfunksjon for plotting
 def apply_mask(triang,p, alpha=0.1):
     # Mask triangles with sidelength bigger some alpha
     triangles = triang.triangles
@@ -254,6 +196,7 @@ def apply_mask(triang,p, alpha=0.1):
     # apply masking
     triang.set_mask(maxi > alpha)
 
+#hjelpefunksjon for generering av stivhets- og divergensmatriser
 def submat(points,el,int_func,multibasis = False):
     if multibasis:
         c1 = phiLin(points,el[0])
@@ -273,6 +216,7 @@ def submat(points,el,int_func,multibasis = False):
                 a_el[i,j] += gauss2D(int_func,points,el[1],i,j,c2 = c2)
     return a_el
 
+#genererer stivhetsmatrisa
 def createA(int_func,points,elements):
     A = sp.lil_matrix((len(points),len(points)))
     for el in elements:
@@ -281,7 +225,8 @@ def createA(int_func,points,elements):
                 for j,aj in enumerate(el[1]):
                     A[ai,aj] += a_el[i,j]
     return A
-    
+
+#genererer divergensmatrisa    
 def createD(int_func,points,elements):
     B = sp.lil_matrix((len(points),len(points)))
     for el in elements:
@@ -291,54 +236,54 @@ def createD(int_func,points,elements):
                 B[ai,aj] += a_el[i,j]
     return B
 
-def createAlpha(int_func,points,elements):
-    i = 0
-    alpha = np.zeros((len(points)))
-    for el in elements:
-        basis = phiLin(points,el[0])
-        for j,aj in enumerate(el[0]):
-            alpha[aj] += gauss2D(int_func,points,el[1],i,j,c2 = basis)
-    return alpha
-
-def createTest(points,N):
-    i = 0
-    r = 1
-    elements = []
-    lin = []
-    sq = []
-    lin_vec = []
-    while r != (2**N)+1:
-        lin = np.asarray([i,i+2,i+(10*(2**N)+2),i+(10*(2**N)+4)])
-        sq = np.asarray([i,i+1,i+2,i+(5*(2**N)+1),i+(5*(2**N)+2),i+(5*(2**N)+3),i+(10*(2**N)+2),i+(10*(2**N)+3),i+(10*(2**N)+4)])
-        el = [lin,sq]
-        elements.append(el)
-        lin_vec = np.concatenate((lin_vec,lin))
-        i += 2
-        if i==(5*(2**N))*r + r -1:
-            i += 5*(2**N) +2
-            r += 2
-    return elements, np.array(list(set(lin_vec)),dtype=int)
-
-def createDomain(N):
+def createDomain(N,typ = 0):
     x = np.linspace(0,1,int(5*(2**N)+1))
-    y = np.linspace(0,.4,int(2*(2**N)+1))
+    y = np.linspace(0,1,int(5*(2**N)+1))
     X,Y = np.meshgrid(x,y)
 
     px = X.ravel()
     py = Y.ravel()
     origpts = np.vstack((px,py)).T
-    points = origpts[np.logical_or(origpts[:,1] <.20005,np.logical_and(origpts[:,0] > .39995,origpts[:,0] < .60005))]
-    return points
+    if typ == 0:
+        points = origpts[origpts[:,1] <.20005]
+        elements,lin_set = typeZeroDom(points,N)
+        non_homog_dir,homog_dir,neumann = typeZeroBdry(points,N)
+    elif typ == 1:
+        points = origpts[np.logical_or(origpts[:,1] <.20005,np.logical_and(np.logical_and(origpts[:,0] > .39995,origpts[:,0] < .60005),origpts[:,1]<.40005))]
+        elements,lin_set = typeOneDom(points,N)
+        non_homog_dir,homog_dir,neumann = typeOneBdry(points,N)
+    return points,elements,lin_set,non_homog_dir,homog_dir, neumann
 
-#definer området:
-N = 4
-x = np.linspace(0,1,int(5*(2**N)+1))
-y = np.linspace(0,.4,int(2*(2**N)+1))
-X,Y = np.meshgrid(x,y)
+#fjerner ønskede rader og kolonner fra inputmatrisa
+def matrixShaver(Mat,rows,cols):
+    Out = Mat[rows]
+    Out = Out[:,cols]
+    return Out
 
-px = X.ravel()
-py = Y.ravel()
+def solHelper(sol,lift,inner,points):
+    uxinner = sol[:len(inner)]
+    uyinner = sol[len(inner):2*len(inner)]
+    p = sol[2*len(inner):]
+    ux = np.zeros(len(points))
+    uy = np.zeros(len(points))
+    ux[inner] = uxinner
+    ux[non_homog] = rg
+    uy[inner] = uyinner
+    return ux,uy,p
 
+def plotHelp(points,lin_set,N):
+    tri1 = mtri.Triangulation(points[:,0],points[:,1])
+    apply_mask(tri1,points,alpha = 0.3/(2**N))
+    lin_pts = points[lin_set]
+    tri2 = mtri.Triangulation(lin_pts[:,0],lin_pts[:,1])
+    apply_mask(tri2,lin_pts,alpha=0.3/(2**(N-1)))
+    return tri1,tri2
+
+#variabler, mu1 er amplitude på hastighetsprofil, mu2 er dynamsik viskositet
+mu1 = 10
+mu2 = 100
+
+#definerer basisfunksjoner
 phi = lambda x,y,c,i: c[i][0] + c[i][1]*x + c[i][2]*y + c[i][3]*x*y + c[i][4]*(x**2) + c[i][5]*(y**2) + c[i][6]*(x**2)*y + c[i][7]*x*(y**2) +c[i][8]*(x**2)*(y**2)
 phi_dx = lambda x,y,c,i: c[i][1] + c[i][3]*y + 2*c[i][4]*x + 2*c[i][6]*x*y + c[i][7]*(y**2) + 2*c[i][8]*x*(y**2)
 phi_dy = lambda x,y,c,i: c[i][2] + c[i][3]*x + 2*c[i][5]*y + c[i][6]*(x**2) + 2*c[i][7]*x*y+ 2*c[i][8]*(x**2)*y
@@ -347,93 +292,51 @@ zeta = lambda x,y,c,i: c[i][0] + c[i][1]*x + c[i][2]*y +c[i][3]*x*y
 zeta_dx = lambda x,y,c,i: c[i][1] + c[i][3]*y
 zeta_dy = lambda x,y,c,i: c[i][2] + c[i][3]*x
 
-mu1 = 10
-mu2 = 100
-
 a_bilin = lambda x,y,c,i,j: (1/mu1)*(phi_dx(x,y,c,j)*phi_dx(x,y,c,i) + phi_dy(x,y,c,j)*phi_dy(x,y,c,i))
 b_bilin_x = lambda x,y,c1,c2,i,j: -phi_dx(x,y,c2,j)*zeta(x,y,c1,i)
 b_bilin_y = lambda x,y,c1,c2,i,j: -phi_dy(x,y,c2,j)*zeta(x,y,c1,i)
-alpha_int = lambda x,y,c,i,j: zeta(x,y,c,j)
 
-origpts = np.vstack((px,py)).T
-points = origpts[np.logical_or(origpts[:,1] <.20005,np.logical_and(origpts[:,0] > .39995,origpts[:,0] < .60005))]
-elements,lin_set = hoodTaylor(points,N)
-non_homog_dir,homog_dir,neumann = boundaries(points,N)
+#bygger likningssystemet
+N = 4
+points,elements,lin_set,non_homog,homog,neu = createDomain(N,typ=0)
 
 A = createA(a_bilin,points,elements)
 Dx = createD(b_bilin_x,points,elements)
 Dy = createD(b_bilin_y,points,elements)
 
-alpha = createAlpha(alpha_int,points,elements)
-alpha = alpha[lin_set]
+#definerer indre noder
+inner = np.array([i for i in range(len(points))])
+inner = inner[~np.isin(inner, np.concatenate((non_homog,homog)))]
 
-inner_sq = np.array([i for i in range(len(points))])
-inner_sq = inner_sq[~np.isin(inner_sq, np.concatenate((non_homog_dir,homog_dir)))]
-outer_sq = np.concatenate((non_homog_dir,homog_dir))
-inner_lin = lin_set[np.isin(lin_set,inner_sq)]
-outer_lin = lin_set[np.isin(lin_set,outer_sq)]
-non_homog_lin = lin_set[np.isin(lin_set,non_homog_dir)]
+#fjerner nødvendige rader og kollonner
+Ai = matrixShaver(A,inner,inner)
+Dxi = matrixShaver(Dx,lin_set,inner)
+Dyi = matrixShaver(Dy,lin_set,inner)
+Gx = matrixShaver(Dx,lin_set,non_homog)
+G = matrixShaver(A,inner,non_homog)
 
-#grenser for trykkrommet:
+#definerer lifting-funksjonen
+y_n = points[non_homog][:,1]
+rg = mu2*y_n*(.2-y_n)
 
-Ai = A[inner_sq]
-Ai = Ai[:,inner_sq]
-Dx = Dx[lin_set]
-Gx = Dx[:,non_homog_dir]
-Dx = Dx[:,inner_sq]
-Dy = Dy[lin_set]
-Gy = Dy[:,non_homog_dir]
-Dy = Dy[:,inner_sq]
-DxT = Dx.T
-DyT = Dy.T
-
-G = A[inner_sq]
-G = G[:,non_homog_dir]
-
-sq_x = points[non_homog_dir][:,1]
-rgsq = mu2*sq_x*(.2-sq_x)
-
-lift_sq = G@rgsq
-lift_lin = Gx@rgsq
-
-fx = np.zeros_like(lift_sq) - lift_sq 
-fy = np.zeros_like(lift_sq)
-fp = np.zeros_like(lift_lin) - lift_lin
-
+#lager høyresiden
+fx = -G@rg
+fy = np.zeros_like(fx)
+fp = -Gx@rg
 rhs = np.concatenate((fx,fy,fp))
-Block = sp.bmat([[Ai,None,DxT],[None,Ai,DyT],[Dx,Dy,None]]).tocsr()
+
+#bygger blokkmatrisa og løser
+Block = sp.bmat([[Ai,None,Dxi.T],[None,Ai,Dyi.T],[Dxi,Dyi,None]]).tocsr()
 u_bar = solver(Block,rhs)
-uxinner = u_bar[:len(inner_sq)]
-uyinner = u_bar[len(inner_sq):2*len(inner_sq)]
-pinner = u_bar[2*len(inner_sq):]
-ux = np.zeros(len(points))
-uy = np.zeros(len(points))
-p = np.zeros(len(lin_set))
-ux[inner_sq] = uxinner
-ux[non_homog_dir] = rgsq
-uy[inner_sq] = uyinner
-p = pinner
+ux,uy,p = solHelper(u_bar,rg,inner,points)
 
-plt.figure(10)
-exx = np.linspace(0,1,len(ux[non_homog_dir]))
-plt.plot(exx,ux[non_homog_dir])
-plt.savefig("hastighetsprofil.png")
-
-#p[1:] = pinner
-
-#p[0] = np.dot(alpha[1:],p[1:])
-#print(np.dot(alpha,p))
 print("average out velocity (x)")
-print(sum(ux[non_homog_dir-1])/len(ux[non_homog_dir-1]))
+print(sum(ux[neu])/len(ux[neu]))
 print("average in velocity (x)")
-print(sum(ux[non_homog_dir])/len(ux[non_homog_dir]))
+print(sum(ux[non_homog])/len(ux[non_homog]))
 
-
-tri1 = mtri.Triangulation(points[:,0],points[:,1])
-apply_mask(tri1,points,alpha = 0.3/(2**N))
-lin_pts = points[lin_set]
-tri2 = mtri.Triangulation(lin_pts[:,0],lin_pts[:,1])
-apply_mask(tri2,lin_pts,alpha=0.3/(2**(N-1)))
+#generer triangulering og maskerer for enklere plotting 
+tri1,tri2 = plotHelp(points,lin_set,N)
 
 plt.figure(0)
 plotElements(points,elements)
